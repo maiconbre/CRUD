0efd3c8fe661b98d36c6414e8baf23a5512ab58d
brwnalima <bruna.lima@edu.unirio.br>
2023-02-15 10:23:20 -0300

    att

--- src/server.js
+++ src/server.js
@@ -4,8 +4,18 @@ const routes = require ('./routes')
 require ('./database');
 
 const app = express();
+const port = 3030
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3030);
+app.listen(port, ()=>{
+    console.log(`Server rodando em: http://localhost:${port}/`)
+})
+
+/* 
+* EXTENSÕES:
+* Thunder Client > pra usar e testar o CRUD
+* JSON Viewrs no Google Chrome > pro JSON ficar bonitinho no navegador
+* SQLITE > App pra ver as tabelas
+*/

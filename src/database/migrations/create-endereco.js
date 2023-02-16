f3ece30e6258ae2a262771c3062e066067540060
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-02-16 02:12:24 -0300

    Removendo mysql

--- src/database/migrations/create-endereco.js
+++ src/database/migrations/create-endereco.js
@@ -32,14 +32,6 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
     });
   },
 

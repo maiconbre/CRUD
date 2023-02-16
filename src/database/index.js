a4239e3578906e37ae7340e18eac593e33a0e9e3
Tamires Porto <tamires.silveira.porto@gmail.com>
2023-02-16 00:13:22 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -4,10 +4,12 @@ const dbConfig = require('../config/database');
 const User = require('../models/User');
 const Endereco = require('../models/Endereco');
 
+
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Endereco.init(connection);
+
 
 User.associate(connection.models);
 Endereco.associate(connection.models);

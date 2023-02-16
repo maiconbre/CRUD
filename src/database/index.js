b7c49568e6fe5c56836617f13841c3241dd6de02
brwnalima <bruna.lima@edu.unirio.br>
2023-02-16 10:14:45 -0300

    tirando arquivos pro pull request

--- src/database/index.js
+++ src/database/index.js
@@ -2,19 +2,16 @@ const Sequelize = require ('sequelize');
 const dbConfig = require('../config/database');
 
 const User = require('../models/User');
-const Consulta = require('../models/Consulta');
 const Cargo = require('../models/Cargo')
 
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
-Consulta.init(connection);
 Cargo.init(connection)
 
 
 User.associate(connection.models);
-Consulta.associate(connection.models);
 Cargo.associate(connection.models)
 
 
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 const express = require ('express');
 
-const ConsultaController = require('./controllers/ConsultaController');
 const UserController = require('./controllers/UserController');
 const CargoController = require('./controllers/CargoController')
 
@@ -11,9 +10,6 @@ routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
-
-routes.post('/users/:user_id/consultas', ConsultaController.store);
-routes.get('/users/:user_id/consultas', ConsultaController.index);
 
 routes.post('/users/:user_id/cargos', CargoController.store);
 routes.get('/users/:user_id/cargos', CargoController.index);

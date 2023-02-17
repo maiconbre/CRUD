d1c542404bb29a226300ea21073a444d84546831
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-02-17 19:18:47 -0300

    Metodo user/id
    Pega uma id especifica e retorna todas as informações relacionadas a esse id

--- src/routes.js
+++ src/routes.js
@@ -8,26 +8,35 @@ const TechController = require('./controllers/TechController')
 
 const routes = express.Router();
 
+//Metodos CRUD dos Usuarios
+routes.get('/users/:id', UserController.indexOne);
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.delete);
 
+//Metodos CRUD dos Endereços
 routes.get('/users/:user_id/enderecos', EnderecoController.index);
 routes.post('/users/:user_id/enderecos', EnderecoController.store);
 routes.put('/users/:user_id/enderecos/:id', EnderecoController.update);
 routes.delete('/users/:user_id/enderecos/:id', EnderecoController.delete);
 
+
+//Metodos CRUD das Sedes
 routes.get('/sedes', SedeController.index);
 routes.post('/sedes', SedeController.store);
 routes.put('/sedes/:id', SedeController.update);
 routes.delete('/sedes/:id', SedeController.delete);
 
+
+//Metodos CRUD dos Cargos
 routes.get('/users/:user_id/cargos', CargoController.index);
 routes.post('/users/:user_id/cargos', CargoController.store);
 routes.put('/users/:user_id/cargos', CargoController.update);
 routes.delete('/users/:user_id/cargos', CargoController.delete);
 
+
+//Metodos CRUD das Tecnologias
 routes.get('/users/:user_id/techs', TechController.index);
 routes.post('/users/:user_id/techs', TechController.store);
 routes.delete('/users/:user_id/techs/:id', TechController.delete);

d8abb5f85e6154e82554cd97b4c045a2ca55bf0b
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2023-02-20 10:49:18 -0300

    Update UserController.js

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -8,6 +8,36 @@ module.exports = {
         return res.json(users);
 
     },
+    async indexOne(req, res){
+        const { id } = req.params;
+      
+        const user = await User.findByPk(id, {
+            include: [
+              { 
+                model: Cargo, 
+                as: 'cargos',
+                attributes: ['profissao'] // somente a coluna "profissao"
+              },
+              { 
+                model: Endereco, 
+                as: 'enderecos',
+                attributes: ['cep','cidade','bairro','estado'] // somente as colunas desejadas
+              },
+              { 
+                model: Tech, 
+                as: 'techs',
+                attributes: ['espec'], //somente a coluna espec
+                through: { attributes: [] } }
+            ]
+        });
+      
+        if (!user) {
+          return res.status(400).json({ error: 'Usuário não encontrado' });
+        }
+      
+        return res.json(user); //Isso irá retornar as informações do usuário e de todas as tabelas relacionadas.
+      },
+      
     async store(req, res) {
         const { nome, email, github } = req.body;
 
